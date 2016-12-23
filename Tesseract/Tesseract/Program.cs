c075c7d8befc3804c321df765855bf919b68a9f4
Anton Turkin <Anton_Turkin@epam.com>
2016-12-23 11:29:28 +0400

    Add Tesseract library sample

---
+++ Tesseract/Tesseract/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Tesseract;
+
+namespace ConsoleApplication4
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var language = @"rus";//@"eng";//@"rus";
+            var text = "No text";
+
+            var blogPostImage = @"C:\2.png";
+
+            using (var ocrEngine = new TesseractEngine(@".\tessdata", language, EngineMode.Default))
+            {
+                using (var imageWithText = Pix.LoadFromFile(blogPostImage))
+                {
+                    using (var page = ocrEngine.Process(imageWithText))
+                    {
+                        text = page.GetText();
+                        Console.WriteLine(text);
+                        Console.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
